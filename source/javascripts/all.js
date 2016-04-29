fae7ea13b2a7b03ac92785f3774f878abcb1e5e2
Andrew Hoglund <ahoglund@gmail.com>
2016-04-29 00:43:49 -0400

    order page done

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,34 +2,35 @@ var CalvaryComics = CalvaryComics || {};
 
 CalvaryComics.books = {
   init: function(base_path) {
+    var self = this;
     image_number = 0;
     $.each($("#preview_images span"), function() {
-      CalvaryComics.books.images.push($(this).attr("data-image-name"))
+      self.images.push($(this).attr("data-image-name"))
     });
     $("#current-image img").attr("src", base_path + CalvaryComics.books.images[0]);
     $("#previous-button").click(function(e) {
       e.preventDefault();
-      CalvaryComics.books.calculate_previous();
-      CalvaryComics.books.update_image_title(image_number);
-      CalvaryComics.books.update_image_src(base_path, image_number);
+      self.calculate_previous();
+      self.update_image_title(image_number);
+      self.update_image_src(base_path, image_number);
     });
     $("#next-button").click(function(e) {
       e.preventDefault();
-      CalvaryComics.books.calculate_next();
-      CalvaryComics.books.update_image_title(image_number);
-      CalvaryComics.books.update_image_src(base_path, image_number);
+      self.calculate_next();
+      self.update_image_title(image_number);
+      self.update_image_src(base_path, image_number);
     });
   },
   images: [],
   calculate_previous: function() {
     image_number = image_number - 1;
     if(image_number < 0) {
-      image_number = CalvaryComics.books.images.length - 1;
+      image_number = this.images.length - 1;
     };
   },
   calculate_next: function() {
     image_number = image_number + 1;
-    if(image_number > CalvaryComics.books.images.length - 1) {
+    if(image_number > this.images.length - 1) {
       image_number = 0;
     }
   },
@@ -38,7 +39,7 @@ CalvaryComics.books = {
     if(img_num == 0) {
       image_title =  "Front Cover"
     }
-    else if(img_num == CalvaryComics.books.images.length - 1) {
+    else if(img_num == this.images.length - 1) {
       image_title = "Back Cover"
     }
     else {
@@ -47,6 +48,6 @@ CalvaryComics.books = {
     $("#current-image-title").html(image_title)
   },
   update_image_src: function(base_path, img_num) {
-    $("#current-image img").attr("src", base_path + CalvaryComics.books.images[img_num]);
+    $("#current-image img").attr("src", base_path + this.images[img_num]);
   },
 };
